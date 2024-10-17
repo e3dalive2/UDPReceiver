546050bdb0fd5509991dcae961cd48e924fc410a
Kifa <e3dalive@gmail.com>
2024-10-17 20:34:49 +0900

    cmd parsing

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <regex>
 
 #include <spdlog/spdlog.h>
 
@@ -5,7 +6,18 @@
 #include "Tools.hpp"
 #include "UDPServer.hpp"
 
-int main()
+void print_help()
+{
+    spdlog::info("Usage: UDPReciever [options]");
+    spdlog::info("Options:");
+    spdlog::info("  --port <port>              Port number (default: 27020)");
+    spdlog::info("  --bindIP <ip_address>      IP address to bind (default: 127.0.0.1)");
+    spdlog::info("  --filter <regex>           Filter string (default: '.*')");
+    spdlog::info("  --recordPath <path>        Path to record file (default: 'records.txt')");
+    spdlog::info("  --help                     Display this help message");
+}
+
+int main(int argc, char *argv[])
 {
 #ifdef WIN32
     SetConsoleOutputCP(CP_UTF8);
@@ -16,12 +28,56 @@ int main()
     std::string filterString = ".*";
     std::string recordPath = "records.txt";
 
+    try
+    {
+        // Command line argument parsing
+        for (int i = 1; i < argc; ++i) {
+            std::string arg = argv[i];
+
+            if (arg == "--help") {
+                print_help();
+                return 0;
+            } else if (arg == "--port" && (i + 1 < argc)) {
+                auto uPort = static_cast<uint16_t>(std::atoi(argv[++i]));
+                if (uPort == 0) {
+                    spdlog::error("Invalid port number provided. Using default: {}", uPort);
+                    port = 27020;
+                }
+                else
+                {
+                    port = uPort;
+                }
+            } else if (arg == "--bindIP" && (i + 1 < argc)) {
+                bindIP = argv[++i];
+            } else if (arg == "--filter" && (i + 1 < argc)) {
+                filterString = argv[++i];
+                try {
+                    std::regex rgx(filterString);
+                } catch (const std::regex_error &e) {
+                    spdlog::error("Invalid regex provided: {}", e.what());
+                    return 1;
+                }
+            } else if (arg == "--recordPath" && (i + 1 < argc)) {
+                recordPath = argv[++i];
+            } else {
+                spdlog::warn("Unknown argument: {}", arg);
+            }
+        }
+    }
+    catch (std::exception e)
+    {
+        spdlog::error("parsing params failed with {}", e.what());
+    }
+
     try {
         asio::io_context io_context;
 
         auto logger = std::make_shared<Logger>(recordPath);
 
         UDPServer server(io_context, bindIP, port); // Listening on port 12345
+        spdlog::info("filter {}", filterString);
+        spdlog::info("output {}", recordPath);
+
         server.setFilter(filterString);
         server.setLogger(logger);
 
