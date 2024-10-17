988b3568fe58152dda1461ac3675a29c5f904da5
Kifa <e3dalive@gmail.com>
2024-10-17 17:01:43 +0900

    testing on socketscpp

--- main.cpp
+++ main.cpp
@@ -3,51 +3,68 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define _SILENCE_ALL_MS_EXT_DEPRECATION_WARNINGS
 #define ENET_IMPLEMENTATION
-#include <enet.h>
-#include <stdio.h>
+#include <sockets-cpp/UdpSocket.h>
 
 #include <spdlog/spdlog.h>
 
-const int kMaxClients = 4000;//enet limitations
+class UnicastApp {
+public:
+	// UDP Multicast
+	UnicastApp(const char* remoteAddr, const char* listenAddr, uint16_t localPort, uint16_t port);
 
-void enet_process()
-{
-	ENetAddress address;
-	ENetHost* server;
+	virtual ~UnicastApp() = default;
 
-	/* Bind the server to the default localhost.     */
-	/* A specific host address can be specified by   */
-	/* enet_address_set_host (& address, "x.x.x.x"); */
+	void onReceiveData(const char* data, size_t size);
 
-	address.host = ENET_HOST_ANY;
-	/* Bind the server to port 1234. */
-	address.port = 27010;
+	void sendMsg(const char* data, size_t len);
 
-	server = enet_host_create(&address /* the address to bind the server host to */,
-		kMaxClients      /* allow up to 32 clients and/or outgoing connections */,
-		1      /* allow up to 2 channels to be used, 0 and 1 */,
-		0      /* assume any amount of incoming bandwidth */,
-		0      /* assume any amount of outgoing bandwidth */);
-	if (server == NULL)
-	{
-		spdlog::error("An error occurred while trying to create an ENet server host");
-		exit(EXIT_FAILURE);
+private:
+	sockets::SocketOpt m_socketOpts;
+	sockets::UdpSocket<UnicastApp> m_unicast;
+};
+
+UnicastApp::UnicastApp(const char* remoteAddr, const char* listenAddr, uint16_t localPort, uint16_t port) : m_socketOpts({ sockets::TX_BUFFER_SIZE, sockets::RX_BUFFER_SIZE, listenAddr }), m_unicast(*this, &m_socketOpts) {
+	sockets::SocketRet ret = m_unicast.startUnicast(remoteAddr, localPort, port);
+	if (ret.m_success) {
+		std::cout << "Listening on UDP " << listenAddr << ":" << localPort << " sending to " << remoteAddr << ":" << port << "\n";
 	}
+	else {
+		std::cout << "Error: " << ret.m_msg << "\n";
+		exit(1); // NOLINT
+	}
+}
+
+void UnicastApp::sendMsg(const char* data, size_t len) {
+	auto ret = m_unicast.sendMsg(data, len);
+	if (!ret.m_success) {
+		std::cout << "Send Error: " << ret.m_msg << "\n";
+	}
+}
+
+void UnicastApp::onReceiveData(const char* data, size_t size) {
+	std::string str(reinterpret_cast<const char*>(data), size);
+
+	std::cout << "Received: " << str << "\n";
 }
 
 int main()
 {
-	spdlog::info("starting");
+	const char* addr = "127.0.0.1";
+	const char* listenAddr = "127.0.0.1";
+	uint16_t port = 27020;
+	uint16_t localPort = 27020;
 
-	if (enet_initialize() != 0) {
-		spdlog::error("An error occurred while initializing ENet");
-		return 1;
+	UnicastApp app(addr, listenAddr, localPort, port);
+
+	while (true) {
+		std::string data;
+		std::cout << "Data >";
+		std::getline(std::cin, data);
+		if (data == "quit") {
+			break;
+		}
+		app.sendMsg(data.data(), data.size());
 	}
-
-	enet_process();
-
-	spdlog::info("closing");
-	enet_deinitialize();
 
 	return 8;
 }
